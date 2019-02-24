989f54b1b89ce62f35280698eedca8eee10b1a9d
Matthew Jadud <matt@jadud.com>
2019-02-24 14:47:00 -0500

    Moving tests into individual files.

--- formats/sql.rkt
+++ formats/sql.rkt
@@ -23,13 +23,13 @@
              (table->scribble (rest t)))]))
 
 (define mysql
-  `( ((integer tinyint smallint mediumint bigint)
+  `( ((int integer tinyint smallint mediumint bigint)
       ,integer-sanitizer)
      ((real double decimal)
       ,number-sanitizer)
      ((varchar text var-string)
       ,string-sanitizer)
-     ((date time datetime)
+     ((datetime timestamp date time)
       ,identity-sanitizer)
      ((blob)
       ,identity-sanitizer)
@@ -46,7 +46,7 @@
      ;; FIXME: blob should result in bytes?
      ((blob)
       ,identity-sanitizer
-     ) )
+      ) )
   )
 
 (define (make-sql-sanitizer table)
@@ -57,7 +57,9 @@
          (error 'sql-type->sanitizer
                 "Cannot find a sanitizer for type [ ~a ]~n"
                 t)]
-        [(member (format "~a" t) (map ~a (first (first table))))
+        [(ormap (λ (type)
+                  (regexp-match type (format "~a" t)))
+                (map ~a (first (first table))))
          (second (first table))]
         [else
          (helper (rest table) t)]))
@@ -70,13 +72,51 @@
   ((make-sql-sanitizer sqlite) t))    
 
 (define (read-mysql conn table)
-   (read-sql conn table #:sql-type->sanitizer mysql-type->sanitizer))
+  (read-sql conn table #:sql-type->sanitizer mysql-type->sanitizer))
 
 (define (read-sqlite conn table)
-   (read-sql conn table
-             #:show-table-query (format "PRAGMA table_info(~a);" table)
-             #:name-column 1
-             #:type-column 2
-             #:sql-type->sanitizer sqlite-type->sanitizer))
+  (read-sql conn table
+            #:show-table-query (format "PRAGMA table_info(~a);" table)
+            #:name-column 1
+            #:type-column 2
+            #:sql-type->sanitizer sqlite-type->sanitizer))
 
+;; ----------------------------------------------------------------- 
+;; ----------------------------- TESTS -----------------------------
+;; -----------------------------------------------------------------
 
+(module+ test
+  (require rackunit
+           "../sanitizers.rkt"
+           "../syntax-wrappers.rkt")
+
+  ;; ----- MySQL -----
+  ;; A public database for testing!
+  ;; https://rfam.readthedocs.io/en/latest/database.html
+  (define conn
+    (mysql-connect
+     #:database "Rfam"
+     #:server "mysql-rfam-public.ebi.ac.uk"
+     #:user "rfamro"
+     #:port 4497))
+  (define T (read-mysql conn "family"))
+  (check-equal? (table-count T) 3016)
+    
+  (check-equal? (table-count
+                 (sieve T
+                        #:using number_of_species
+                        #:where (< number_of_species 5)))
+                546)
+
+  ;; ----- SQLite -----
+  (define connSQLite
+    (sqlite3-connect #:database "sqlite-test-file.sqlite"))
+  (define T1 (read-sqlite connSQLite "error_quotients"))
+  (define T2 (read-sqlite connSQLite "watwin_pairs"))
+  (check-equal? (table-count T1) 3)
+  (check-equal? (table-count T2) 51)
+  (check-equal? (table-count (sieve T2
+                                    #:using id
+                                    #:where (> id 2707309142)))
+                10)
+  )
